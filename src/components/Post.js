e99c2f0b9cf7963b2c2d366bdb94807927d5c813
Omar Qattam <omarqattam99@gmail.com>
2022-09-17 17:39:16 +0300

    post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,62 +1,70 @@
-import React from 'react'
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import {useState, useEffect} from "react";
-import axios from 'axios';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Post(props) {
   const [posts, setPosts] = useState([]);
-  // const [showPostComponent, setshowPostComponent] = useState(false);
+  const [showPostComponent, setShowPostComponent] = useState(false);
 
+  const allPost = "http://localhost:3000/";
   const getAllPost = async () => {
-    const allPost = await axios.get('http://localhost:3000/post');
-    setPosts(allPost.data);
-    // setshowPostComponent(true)
-  }
+    axios
+      .get(`${allPost}getPostComment`)
+      .then((response) => {
+        const allPosts = response.data;
+        console.log(allPosts);
+        setPosts(allPosts);
+        setShowPostComponent(true);
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  };
 
   useEffect(() => {
-    getAllPost()
-  }, [])
+    getAllPost();
+  }, []);
 
   return (
     <div>
-       <Form >
-      <Form.Group>
-        <Form.Label>Post</Form.Label>
-        <Form.Control type="text" name='postName' />
-      </Form.Group>
+      <Form>
+        <Form.Group>
+          <Form.Label>Post</Form.Label>
+          <Form.Control type="text" name="postName" />
+        </Form.Group>
 
-      <Form.Group>
-        <Form.Label>Comment</Form.Label>
-        <Form.Control type="text" name='commentName' />
-      </Form.Group>
+        <Form.Group>
+          <Form.Label>Comment</Form.Label>
+          <Form.Control type="text" name="commentName" />
+        </Form.Group>
 
-      <Button variant="outline-dark" type="submit" value='Add Post'>
-        Submit
-      </Button>
-      
-      <Form.Group>
-      <Form.Label >Post </Form.Label>
-      </Form.Group>
-      <Form.Group>
-      <Form.Label >Comment </Form.Label>
-      </Form.Group>
-    </Form>
+        <Button variant="outline-dark" type="submit" value="Add Post">
+          Submit
+        </Button>
 
-  
-      { posts &&
-        posts.map((pos, idx) => {
-            return (
-                 <div key={idx}>
-                    <p>{pos.post}</p>
-                    <p>{pos.postStatus}</p>
-                 </div>
-            )
-        })
-      }
+        <Form.Group>
+          <Form.Label>Post </Form.Label>
+          {showPostComponent &&
+            posts.map((posts, idx) => {
+              return (
+                <div key={idx}>
+                  <p>{posts.post}</p>
+                  <Form.Group>
+                    <Form.Label>Comment </Form.Label>
+                  </Form.Group>
+                  {showPostComponent &&
+                    posts.Comments.map((comment, idx) => {
+                      return (
+                        <div key={idx}>
+                          <p>{comment.commentWriter}</p>
+                        </div>
+                      );
+                    })}
+                </div>
+              );
+            })}
+        </Form.Group>
+      </Form>
     </div>
-  )
+  );
 }
-
- 
